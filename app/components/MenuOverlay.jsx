a30ab2aa08272fd8c8fe5a3451c01ac5e475fdf0
ImansiKavindi <kavindikulathunga203@gmail.com>
2025-01-20 23:34:44 +0530

    navbar

--- app/components/MenuOverlay.jsx
+++ app/components/MenuOverlay.jsx
@@ -1,33 +1,15 @@
-"use client";
+import React from "react";
 import NavLink from "./NavLink";
-import { XMarkIcon } from "@heroicons/react/24/solid";
 
-const MenuOverlay = ({ links, setNavbarOpen }) => {
+const MenuOverlay = ({ links }) => {
   return (
-    <div className="fixed inset-0 bg-black bg-opacity-90 z-20 flex flex-col items-center justify-center">
-      {/* Close Button */}
-      <div className="absolute top-4 right-4">
-        <button
-          onClick={() => setNavbarOpen(false)}
-          className="text-white hover:text-gray-300"
-        >
-          <XMarkIcon className="h-6 w-6" />
-        </button>
-      </div>
-      {/* Links */}
-      <ul className="space-y-6">
-        {links.map((link, index) => (
-          <li key={index}>
-            <NavLink
-              href={link.path}
-              title={link.title}
-              className="text-2xl text-white hover:text-gray-300 transition"
-              onClick={() => setNavbarOpen(false)}
-            />
-          </li>
-        ))}
-      </ul>
-    </div>
+    <ul className="flex flex-col py-4 items-center">
+      {links.map((link, index) => (
+        <li key={index}>
+          <NavLink href={link.path} title={link.title} />
+        </li>
+      ))}
+    </ul>
   );
 };
 
--- app/components/Navbar.jsx
+++ app/components/Navbar.jsx
@@ -1,76 +1,71 @@
 "use client";
-import { useState } from "react";
+import Link from "next/link";
+import React, { useState } from "react";
+import NavLink from "./NavLink";
 import { Bars3Icon, XMarkIcon } from "@heroicons/react/24/solid";
+import MenuOverlay from "./MenuOverlay";
 
-const NavBar = () => {
+const navLinks = [
+  {
+    title: "About",
+    path: "#about",
+  },
+  {
+    title: "Skills",
+    path: "#skills",
+  },
+  {
+    title: "Projects",
+    path: "#projects",
+  },
+  {
+    title: "Contact",
+    path: "#contact",
+  },
+];
+
+const Navbar = () => {
   const [navbarOpen, setNavbarOpen] = useState(false);
 
-  const links = [
-    { title: "Home", path: "/" },
-    { title: "About", path: "/about" },
-    { title: "Services", path: "/services" },
-    { title: "Contact", path: "/contact" },
-  ];
-
   return (
-    <nav className="text-white fixed top-0 left-0 w-full shadow-md z-50">
-      <div className="container mx-auto flex justify-between items-center px-4 py-3">
-        {/* Logo */}
-        <div className="text-xl font-bold">
-          <a href="/"></a>
-        </div>
-
-        {/* Hamburger Menu for Mobile */}
-        <div className="lg:hidden">
-          <button
-            onClick={() => setNavbarOpen(!navbarOpen)}
-            aria-label="Toggle menu"
-          >
-            {navbarOpen ? (
-              <XMarkIcon className="h-6 w-6 text-white" />
-            ) : (
-              <Bars3Icon className="h-6 w-6 text-white" />
-            )}
-          </button>
-        </div>
-
-        {/* Links */}
-        <ul
-          className={`lg:flex space-x-6 hidden ${
-            navbarOpen ? "block absolute top-full left-0  w-full" : ""
-          }`}
+    <nav className="fixed mx-auto border border-[#33353F] top-0 left-0 right-0 z-10 bg-[#121212] bg-opacity-100">
+      <div className="flex container lg:py-4 flex-wrap items-center justify-between mx-auto px-4 py-2">
+        <Link
+          href={"/"}
+          className="text-2xl md:text-5xl text-white font-semibold"
         >
-          {links.map((link, index) => (
-            <li key={index} className="py-2 lg:py-0 text-center">
-              <a
-                href={link.path}
-                className="hover:text-gray-300 block px-4 lg:inline"
-              >
-                {link.title}
-              </a>
-            </li>
-          ))}
-        </ul>
+         
+        </Link>
+        <div className="mobile-menu block md:hidden">
+          {!navbarOpen ? (
+            <button
+              onClick={() => setNavbarOpen(true)}
+              className="flex items-center px-3 py-2 border rounded border-slate-200 text-slate-200 hover:text-white hover:border-white"
+            >
+              <Bars3Icon className="h-5 w-5" />
+            </button>
+          ) : (
+            <button
+              onClick={() => setNavbarOpen(false)}
+              className="flex items-center px-3 py-2 border rounded border-slate-200 text-slate-200 hover:text-white hover:border-white"
+            >
+              <XMarkIcon className="h-5 w-5" />
+            </button>
+          )}
+        </div>
+        <div className="menu hidden md:block md:w-auto" id="navbar">
+          <ul className="flex p-4 md:p-0 md:flex-row md:space-x-8 mt-0">
+            {navLinks.map((link, index) => (
+              <li key={index}>
+                <NavLink href={link.path} title={link.title} />
+              </li>
+            ))}
+          </ul>
+        </div>
       </div>
-
-      {/* Mobile Dropdown Menu */}
-      {navbarOpen && (
-        <ul className="lg:hidden  text-white">
-          {links.map((link, index) => (
-            <li key={index} className="py-2 text-center border-t border-white">
-              <a
-                href={link.path}
-                className="hover:text-gray-300 block"
-                onClick={() => setNavbarOpen(false)}
-              >
-                {link.title}
-              </a>
-            </li>
-          ))}
-        </ul>
-      )}
+      {navbarOpen ? <MenuOverlay links={navLinks} /> : null}
     </nav>
   );
 };
 
-export default NavBar;
+export default Navbar;
