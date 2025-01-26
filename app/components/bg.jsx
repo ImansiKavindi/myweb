1c009f51cab39e1f1355ffee546d1caec6d410f2
ImansiKavindi <kavindikulathunga203@gmail.com>
2025-01-26 11:35:09 +0530

    .

--- app/components/bg.jsx
+++ app/components/bg.jsx
@@ -1,96 +1,17 @@
-'use client';
+import Starfield from 'react-starfield';
 
-import React from 'react';
-
-const Bg = () => {
-  const [stars, setStars] = React.useState([]);
-
-  React.useEffect(() => {
-    const generateStars = () => {
-      const isMobile = window.matchMedia('(max-width: 768px)').matches; // Adjust breakpoint as needed
-      const numberOfStars = isMobile ? 15: 30; // Fewer stars on mobile
-      const generatedStars = [...Array(numberOfStars)].map(() => ({
-        top: Math.random() * 100,
-        left: Math.random() * 100,
-        animationDuration: Math.random() * 5 + 3,
-        animationDelay: Math.random() * 5,
-      }));
-      setStars(generatedStars);
-    };
-
-    // Generate stars initially
-    generateStars();
-
-    // Add event listener for screen resizing
-    const resizeListener = () => generateStars();
-    window.addEventListener('resize', resizeListener);
-
-    return () => {
-      window.removeEventListener('resize', resizeListener);
-    };
-  }, []);
-
+function Starfiled() {
   return (
-    <div className="starry-background">
-      {/* Stars */}
-      {stars.map((star, i) => (
-        <div
-          key={i}
-          className={`star ${i % 2 === 0 ? 'big-star' : 'small-star'}`}
-          style={{
-            top: `${star.top}%`,
-            left: `${star.left}%`,
-            animationDuration: `${star.animationDuration}s`,
-            animationDelay: `${star.animationDelay}s`,
-          }}
-        ></div>
-      ))}
-
-      <style jsx>{`
-        .starry-background {
-          position: fixed;
-          top: 0;
-          left: 0;
-          width: 100%;
-          height: 100%;
-          z-index: -1;
-          pointer-events: none;
-          background-color: black;
-        }
-
-        .star {
-          position: absolute;
-          border-radius: 50%;
-          background-color: white;
-          opacity: 0;
-          animation: twinkle infinite;
-        }
-
-        .big-star {
-          width: 4px;
-          height: 4px;
-        }
-
-        .small-star {
-          width: 3px;
-          height: 3px;
-        }
-
-        @keyframes twinkle {
-          0% {
-            opacity: 0;
-          }
-          50% {
-            opacity: 1;
-            box-shadow: 0 0 8px rgba(255, 255, 255, 0.8);
-          }
-          100% {
-            opacity: 0;
-          }
-        }
-      `}</style>
+    <div className="App">
+      <Starfield
+        starCount={1000}
+        starColor={[255, 255, 255]}
+        speedFactor={0.05}
+        backgroundColor="black"
+      />
+    
     </div>
   );
-};
+}
 
-export default Bg;
+export default Starfiled;
